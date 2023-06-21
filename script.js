b5a55b17958f2800a03de04166c5f6dffe3662e5
Sachet Subedi <sachetsubedi001@gmail.com>
2023-06-21 18:45:49 +0545

    updates

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 var equals=document.getElementById('equals');
 equals.addEventListener('click',equalsCalc);
+var onBtn=document.querySelector('.onBtn'); 
+var running=false;
 
+onBtn.addEventListener('click',()=>{
+    running=!running;
+    if (running){
+        onBtn.style.backgroundColor="Orange";
+        onBtn.innerHTML="OFF"
+    }
+    else{
+        onBtn.style.backgroundColor="green";
+        onBtn.innerHTML="ON";
+    }
+})
+
+
+function displayAdder(a){
+    var display=document.getElementById('display');
+}
 function equalsCalc()
 {
     var display=document.getElementById('display');
